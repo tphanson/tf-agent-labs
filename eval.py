a1dba30fe03c9eab62e0a2a8468124f49f0d4d54
tphanson <tphanson@kambria.io>
2020-12-21 15:39:07 +0700

    tuning

--- eval.py
+++ eval.py
@@ -4,16 +4,20 @@ import matplotlib.pyplot as plt
 class ExpectedReturn:
     def __init__(self):
         self.returns = None
+        self.max_steps = 500
 
     def compute_avg_return(self, tfenv, agent, num_episodes):
         total_return = 0.0
         for _ in range(num_episodes):
             time_step = tfenv.reset()
+            steps = self.max_steps
             episode_return = 0.0
             while not time_step.is_last():
+                steps -= 1
                 action_step = agent.action(time_step)
                 time_step = tfenv.step(action_step.action)
                 episode_return += time_step.reward
+            episode_return += time_step.reward*steps # Amplify the return
             total_return += episode_return
         avg_return = total_return / num_episodes
         return avg_return.numpy()[0]
