bf9f96ccdbe40f23f929ae837810c583fbe7a0d2
tphanson <tphanson@kambria.io>
2020-12-24 20:30:23 +0700

    tuning

--- buffer.py
+++ buffer.py
@@ -1,17 +1,23 @@
+import tensorflow as tf
+import cv2 as cv
 from tf_agents.replay_buffers import tf_uniform_replay_buffer
 from tf_agents.trajectories import trajectory
-import cv2 as cv
 
 
 class ReplayBuffer:
     def __init__(self, data_spec, batch_size=1):
         self.data_spec = data_spec
         self.batch_size = batch_size
-        self.replay_buffer_capacity = 100000
+        self.replay_buffer_capacity = 100
         self.buffer = tf_uniform_replay_buffer.TFUniformReplayBuffer(
             data_spec=self.data_spec,
             batch_size=self.batch_size,
             max_length=self.replay_buffer_capacity)
+        # For Experience Filtering (EF)
+        self.sub_buffer = tf_uniform_replay_buffer.TFUniformReplayBuffer(
+            data_spec=self.data_spec,
+            batch_size=self.batch_size,
+            max_length=1000)
 
     def __len__(self):
         return self.buffer.num_frames()
@@ -19,44 +25,43 @@ class ReplayBuffer:
     def clear(self):
         return self.buffer.clear()
 
-    def gather_all(self):
-        """ For REINFORCE """
-        return self.buffer.gather_all()
-
     def as_dataset(self, sample_batch_size=64):
-        """ For DQN """
         return self.buffer.as_dataset(
             num_parallel_calls=3,
             sample_batch_size=sample_batch_size,
             num_steps=2
         ).prefetch(3)
 
+    def _filtering(self, traj):
+        # cv.imshow('OhmniInSpace-v0', traj.observation.numpy()[0])
+        # cv.waitKey(10)
+        if self.sub_buffer.num_frames() < 2:
+            return False
+        batch = tf.squeeze(self.sub_buffer.gather_all().observation)
+        batch_observation = tf.tile(
+            traj.observation, [len(batch), 1, 1, 1])
+        min_distance = tf.reduce_min(tf.reduce_sum(
+            tf.square(batch_observation-batch), axis=[-3, -2, -1]))
+        threshold = 100
+        if min_distance > threshold:
+            return False
+        else:
+            return True
+
     def collect(self, env, policy):
-        time_step = env.current_time_step()
-        action_step = policy.action(time_step)
-
-        # Debug
-        img = time_step.observation.numpy()[0]
-        action = action_step.action
-        print(action)
-        cv.imshow('Debug', img)
-        cv.waitKey(10)
-
-        next_time_step = env.step(action_step.action)
-        traj = trajectory.from_transition(
-            time_step, action_step, next_time_step)
-        self.buffer.add_batch(traj)
-        return traj
+        while True:
+            time_step = env.current_time_step()
+            action_step = policy.action(time_step)
+            next_time_step = env.step(action_step.action)
+            traj = trajectory.from_transition(
+                time_step, action_step, next_time_step)
+            if not self._filtering(traj):
+                self.buffer.add_batch(traj)
+                self.sub_buffer.add_batch(traj)
+                return traj
 
     def collect_steps(self, env, policy, steps=1):
-        """ For DQN """
         for _ in range(steps):
-            self.collect(env, policy)
-
-    def collect_episode(self, env, policy, epochs=32):
-        """ For REINFORCE """
-        counter = 0
-        while counter < epochs:
             traj = self.collect(env, policy)
             if traj.is_boundary():
-                counter += 1
+                self.sub_buffer.clear()
