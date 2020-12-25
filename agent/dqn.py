78af09f13cf16a78ef6e07186499a2b19ee50004
tphanson <tphanson@kambria.io>
2020-12-25 14:51:10 +0700

    tuning

--- agent/dqn.py
+++ agent/dqn.py
@@ -3,6 +3,7 @@ from tensorflow import keras
 from tf_agents.agents import dqn
 from tf_agents.networks import q_network
 from tf_agents.utils import common
+from tf_agents.experimental.train.utils import strategy_utils
 
 
 class DQN():
@@ -10,28 +11,30 @@ class DQN():
         # Env
         self.env = env
         # Policy
-        self.preprocessing_layers = keras.Sequential([  # (96, 96, *)
-            keras.layers.Conv2D(  # (92, 92, 16)
-                filters=16, kernel_size=(5, 5), strides=(1, 1), activation='relu'),
-            keras.layers.MaxPooling2D((2, 2)),  # (46, 46, 16)
-            keras.layers.Conv2D(  # (42, 42, 32)
-                filters=32, kernel_size=(5, 5), strides=(1, 1), activation='relu'),
-            keras.layers.MaxPooling2D((2, 2)),  # (21, 21, 32)
-            keras.layers.Conv2D(  # (10, 10, 64)
-                filters=64, kernel_size=(3, 3), strides=(2, 2), activation='relu'),
-            keras.layers.MaxPooling2D((2, 2)),  # (5, 5, 64)
-            keras.layers.Flatten(),
-            keras.layers.Dense(768, activation='relu'),
-        ])
-        self.q_net = q_network.QNetwork(
-            self.env.observation_spec(),
-            self.env.action_spec(),
-            preprocessing_layers=self.preprocessing_layers,
-            fc_layer_params=(512, 256))
-        # Agent
+        self.strategy = strategy_utils.get_strategy(tpu=False, use_gpu=True)
+        with self.strategy.scope():
+            self.preprocessing_layers = keras.Sequential([  # (96, 96, *)
+                keras.layers.Conv2D(  # (92, 92, 16)
+                    filters=16, kernel_size=(5, 5), strides=(1, 1), activation='relu'),
+                keras.layers.MaxPooling2D((2, 2)),  # (46, 46, 16)
+                keras.layers.Conv2D(  # (42, 42, 32)
+                    filters=32, kernel_size=(5, 5), strides=(1, 1), activation='relu'),
+                keras.layers.MaxPooling2D((2, 2)),  # (21, 21, 32)
+                keras.layers.Conv2D(  # (10, 10, 64)
+                    filters=64, kernel_size=(3, 3), strides=(2, 2), activation='relu'),
+                keras.layers.MaxPooling2D((2, 2)),  # (5, 5, 64)
+                keras.layers.Flatten(),
+                keras.layers.Dense(768, activation='relu'),
+            ])
+            self.q_net = q_network.QNetwork(
+                self.env.observation_spec(),
+                self.env.action_spec(),
+                preprocessing_layers=self.preprocessing_layers,
+                fc_layer_params=(512, 256))
+            # Agent
         self.global_step = tf.compat.v1.train.get_or_create_global_step()
         self.optimizer = tf.compat.v1.train.AdamOptimizer(
-            learning_rate=0.00001) # 0.000001
+            learning_rate=0.00001)  # 0.000001
         self.agent = dqn.dqn_agent.DqnAgent(
             self.env.time_step_spec(),
             self.env.action_spec(),
