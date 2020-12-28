42a6815b9ba6bce91fbb6d84585b3908c0296527
tphanson <tphanson@kambria.io>
2020-12-28 11:50:45 +0700

    debug

--- train.py
+++ train.py
@@ -22,55 +22,55 @@ POLICY_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)),
 CHECKPOINT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                               './models/checkpoints')
 
-with tf.device('/device:GPU:0'):
-    # Environment
-    train_env = OhmniInSpace.env()
-    eval_env = OhmniInSpace.env()
+with tf.device('/device:GPU:1'):
+# Environment
+train_env = OhmniInSpace.env()
+eval_env = OhmniInSpace.env()
 
-    # Agent
-    dqn = DQN(train_env, CHECKPOINT_DIR)
-    dqn.agent.train = common.function(dqn.agent.train)
+# Agent
+dqn = DQN(train_env, CHECKPOINT_DIR)
+dqn.agent.train = common.function(dqn.agent.train)
 
-    # Metrics and Evaluation
-    criterion = ExpectedReturn()
+# Metrics and Evaluation
+criterion = ExpectedReturn()
 
-    # Replay buffer
-    initial_collect_steps = 1000
-    replay_buffer = ReplayBuffer(
-        dqn.agent.collect_data_spec,
-        batch_size=train_env.batch_size,
-    )
-    for _ in range(initial_collect_steps):
-        replay_buffer.collect_steps(train_env, dqn.agent.collect_policy)
-    dataset = replay_buffer.as_dataset()
-    iterator = iter(dataset)
+# Replay buffer
+initial_collect_steps = 1000
+replay_buffer = ReplayBuffer(
+    dqn.agent.collect_data_spec,
+    batch_size=train_env.batch_size,
+)
+for _ in range(initial_collect_steps):
+    replay_buffer.collect_steps(train_env, dqn.agent.collect_policy)
+dataset = replay_buffer.as_dataset()
+iterator = iter(dataset)
 
-    # Train
-    num_iterations = 200000
-    eval_step = 1000
-    start = time.time()
-    loss = 0
+# Train
+num_iterations = 200000
+eval_step = 1000
+start = time.time()
+loss = 0
+step = dqn.agent.train_step_counter.numpy()
+while step <= num_iterations:
+    if LOCAL:
+        train_env.render()
+    replay_buffer.collect_steps(train_env, dqn.agent.collect_policy)
+    experience, _ = next(iterator)
+    loss += dqn.agent.train(experience).loss
+    # Evaluation
     step = dqn.agent.train_step_counter.numpy()
-    while step <= num_iterations:
-        if LOCAL:
-            train_env.render()
-        replay_buffer.collect_steps(train_env, dqn.agent.collect_policy)
-        experience, _ = next(iterator)
-        loss += dqn.agent.train(experience).loss
+    if step % eval_step == 0:
+        # Checkpoints
+        dqn.save_checkpoint()
         # Evaluation
-        step = dqn.agent.train_step_counter.numpy()
-        if step % eval_step == 0:
-            # Checkpoints
-            dqn.save_checkpoint()
-            # Evaluation
-            avg_return = criterion.eval(eval_env, dqn.agent.policy, num_episodes=3)
-            print('Step = {0}: Average Return = {1} / Average Loss = {2}'.format(
-                step, avg_return, loss/eval_step))
-            end = time.time()
-            print('Step estimated time: {:.4f}'.format((end-start)/eval_step))
-            # Reset
-            start = time.time()
-            loss = 0
+        avg_return = criterion.eval(eval_env, dqn.agent.policy, num_episodes=3)
+        print('Step = {0}: Average Return = {1} / Average Loss = {2}'.format(
+            step, avg_return, loss/eval_step))
+        end = time.time()
+        print('Step estimated time: {:.4f}'.format((end-start)/eval_step))
+        # Reset
+        start = time.time()
+        loss = 0
 
-    # Visualization
-    criterion.save()
+# Visualization
+criterion.save()
