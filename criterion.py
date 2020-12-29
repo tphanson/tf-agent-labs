e192b20ddd91c4065a8fbf257e089f6c8842302b
tphanson <tphanson@kambria.io>
2020-12-29 13:05:19 +0700

    debug

--- criterion.py
+++ criterion.py
@@ -2,34 +2,28 @@ import matplotlib.pyplot as plt
 from multiprocessing import Pool
 
 
-def eval_single_episode(max_steps, gen_tfenv_func, agent):
-    tfenv = gen_tfenv_func()
-    time_step = tfenv.reset()
-    steps = max_steps
-    episode_return = 0.0
-    while not time_step.is_last():
-        steps -= 1
-        action_step = agent.action(time_step)
-        time_step = tfenv.step(action_step.action)
-        episode_return += time_step.reward.numpy()[0]
-    episode_return += time_step.reward*steps  # Amplify the return
-    return episode_return
-
-
 class ExpectedReturn:
     def __init__(self):
         self.returns = None
         self.max_steps = 500
+    
+    def eval_single_episode(self, tfenv, agent):
+        time_step = tfenv.reset()
+        steps = self.max_steps
+        episode_return = 0.0
+        while not time_step.is_last():
+            steps -= 1
+            action_step = agent.action(time_step)
+            time_step = tfenv.step(action_step.action)
+            episode_return += time_step.reward.numpy()[0]
+        episode_return += time_step.reward*steps  # Amplify the return
+        return episode_return
 
-    def eval_multiple_episodes(self, gen_tfenv_func, agent, num_episodes):
-        pool = Pool()
-        args = []
+    def eval_multiple_episodes(self, tfenv, agent, num_episodes):
+        total_returns = 0.0
         for _ in range(num_episodes):
-            args.append((self.max_steps, gen_tfenv_func, agent))
-        print(args)
-        episode_returns = pool.map(eval_single_episode, args)
-        print(episode_returns)
-        avg_return = sum(episode_returns) / num_episodes
+            total_returns += self.eval_single_episode(tfenv, agent)
+        avg_return = total_returns / num_episodes
         return avg_return
 
     def eval(self, gen_tfenv_func, agent, num_episodes=5):
