b3d07988f38b06c0d0e8995ccdc7c64ff267fca1
tphanson <tphanson@kambria.io>
2021-01-07 14:34:04 +0700

    bugfix

--- agent/dqn.py
+++ agent/dqn.py
@@ -73,6 +73,7 @@ class DQN():
             keras.layers.Dense(768, activation='relu'),
         ])
         zeros = tf.zeros([256])
+        print(zeros)
         v = feed([zeros])
         # Combiner
         conc = keras.layers.Concatenate()
