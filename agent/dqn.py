bf68d4167f09f2a4fc6f2008403cc0569075735f
tphanson <tphanson@kambria.io>
2021-01-07 15:36:20 +0700

    bugfix

--- agent/dqn.py
+++ agent/dqn.py
@@ -55,7 +55,6 @@ class DQN():
         x = keras.layers.Input(shape=(96, 96, 3), batch_size=batch_size)
         # Convolutional layer
         conv = keras.Sequential([  # (96, 96, *)
-            keras.layers.Input(shape=(96, 96, 3), batch_size=batch_size),
             keras.layers.Conv2D(  # (92, 92, *)
                 filters=32, kernel_size=(5, 5), strides=(1, 1), activation='relu'),
             keras.layers.MaxPooling2D((2, 2)),  # (46, 46, *)
@@ -69,9 +68,9 @@ class DQN():
             keras.layers.Dense(768, activation='relu'),
         ])
         x = conv(x)
+        print(x)
         # Feedback layer
         feed = keras.Sequential([
-            keras.layers.Input(shape=(256,), batch_size=batch_size),
             keras.layers.Dense(768, activation='relu'),
             keras.layers.Dense(768, activation='relu'),
         ])
